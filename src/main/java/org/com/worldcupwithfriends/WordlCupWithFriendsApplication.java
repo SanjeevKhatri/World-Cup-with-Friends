b9f28880e0f756375ec7fff25eacab4d87a8a9ef
SanjeevKhatri <sanjeevkhatri29@gmail.com>
2018-01-07 23:38:05 -0600

    creates basic springboot app using gradle with web and lombok

---
+++ src/main/java/org/com/worldcupwithfriends/WordlCupWithFriendsApplication.java
@@ -0,0 +1,12 @@
+package org.com.worldcupwithfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlCupWithFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordlCupWithFriendsApplication.class, args);
+	}
+}
